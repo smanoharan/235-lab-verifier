557778d4099ae34d7e7a3acb1e62aa8dc0b5c8c8
Siva Manoharan <siva@linux-mint-vm.(none)>
2012-09-29 15:16:55 +1200

    Clean up: removed unnecessary code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -18,11 +18,12 @@ public class Application extends Controller
     private static RunResults toResult(Machine machine, String type) throws Exception
     {
       String machinePath = machine.file.getFile().toString();
-      String testPath = "app/t" + type + ".txt";
+      String testPath = "app/inputs/t" + type + ".txt";
       return Tester.testMachine(machinePath, testPath, type);
     }
 
-    public static void checkAll(Machine m11, Machine m12, Machine m13, Machine m21, Machine m22, Machine m31, Machine m32, Machine m41)
+    public static void checkAll(Machine m11, Machine m12, Machine m13, Machine m21, Machine m22, 
+				Machine m31, Machine m32, Machine m41)
     {
       Machine[] machines = new Machine[] { m11, m12, m13, m21, m22, m31, m32, m41 };
       String[] mtypes = new String[] {  "1.1", "1.2", "1.3", "2.1", "2.2", "3.1", "3.2", "4.1" };
@@ -34,10 +35,21 @@ public class Application extends Controller
         if (machines[i] != null && machines[i].file != null)
         {
           count++;
-          try { allResults.add(toResult(machines[i], mtypes[i])); }
-          catch (Exception e) { renderText("index: " + i + ", qs: " + mtypes[i] + ", error: " + e.getMessage()); }
+          try 
+          { 
+            allResults.add(toResult(machines[i], mtypes[i])); 
+            if (i == machines.length - 1)
+            {
+              // allResults.add(checkAlphabetAndEndStates) TODO
+            }
+          }
+          catch (Exception e) 
+          { 
+            renderText("index: " + i + ", qs: " + mtypes[i] + ", error: " + e.getMessage()); 
+          }
         }
       }
+
       if (count > 0) render("Application/results.html", allResults); 
       else renderText("Error: Please upload at least one Turing Machine to verify.");
     }
